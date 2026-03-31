833f8ef981b2a54484e7c4ba2e66f90dcad20f52
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-31 11:40:28 +0200

    fea(home) : add NavbarSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import NavbarSecion from "@/components/organisms/NavbarSecion";
 
 export default function Home() {
   return (
     <>
-     <div className="">
-      page
+     <div className="h-screen">
+      <NavbarSecion />
      </div>
     </>
   );
